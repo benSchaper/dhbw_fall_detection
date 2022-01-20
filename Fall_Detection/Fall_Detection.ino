7d30dd4853c09d47fd44086fd996e98343313e6d
Ben Schaper <ben.schaper@ibm.com>
2022-01-20 10:49:40 +0100

    update arduino code and model

--- Fall_Detection/Fall_Detection.ino
+++ Fall_Detection/Fall_Detection.ino
@@ -1,23 +1,17 @@
 /*
-  IMU Classifier
-
-  This example uses the on-board IMU to start reading acceleration and gyroscope
-  data from on-board IMU, once enough samples are read, it then uses a
-  TensorFlow Lite (Micro) model to try to classify the movement as a known gesture.
-
-  Note: The direct use of C/C++ pointers, namespaces, and dynamic memory is generally
-        discouraged in Arduino examples, and in the future the TensorFlowLite library
-        might change to make the sketch simpler.
-
+  Battery Monitor
+  This example creates a BLE peripheral with the standard battery service and
+  level characteristic. The A0 pin is used to calculate the battery level.
   The circuit:
-  - Arduino Nano 33 BLE or Arduino Nano 33 BLE Sense board.
-
-  Created by Don Coleman, Sandeep Mistry
-  Modified by Dominic Pajak, Sandeep Mistry
-
+  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+  You can use a generic BLE central app, like LightBlue (iOS and Android) or
+  nRF Connect (Android), to interact with the services and characteristics
+  created in this sketch.
   This example code is in the public domain.
 */
 
+#include <ArduinoBLE.h>
 #include <Arduino_LSM9DS1.h>
 
 #include <TensorFlowLite.h>
@@ -28,6 +22,19 @@
 #include <tensorflow/lite/version.h>
 
 #include "model.h"
+
+// BLE SETUP
+
+ // BLE Fall Detection
+BLEService fallDetectionService("a4609aaa-7534-11ec-90d6-0242ac120003");
+
+// BLE Battery Level Characteristic
+BLEFloatCharacteristic probabilityFloat("ee4efbdc-7536-11ec-90d6-0242ac120003",  // standard 16-bit characteristic UUID
+    BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
+
+long previousMillis = 0;  // last time the battery level was checked, in ms
+
+// FALL DETECTION SETUP
 
 const float accelerationThreshold = 2.5; // threshold of significant in G's
 const int numSamples = 357;
@@ -60,16 +67,26 @@ const char* GESTURES[] = {
 #define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);    // initialize serial communication
   while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+
+  // begin initialization
 
   // initialize the IMU
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
+  
+  else if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
 
-  // print out the samples rates of the IMUs
+    while (1);
+  }
+
+   // print out the samples rates of the IMUs
   Serial.print("Accelerometer sample rate = ");
   Serial.print(IMU.accelerationSampleRate());
   Serial.println(" Hz");
@@ -95,64 +112,114 @@ void setup() {
   // Get pointers for the model's input and output tensors
   tflInputTensor = tflInterpreter->input(0);
   tflOutputTensor = tflInterpreter->output(0);
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setLocalName("FallDetectionWristband");
+  BLE.setAdvertisedService(fallDetectionService); // add the service UUID
+  fallDetectionService.addCharacteristic(probabilityFloat); // add the fall propability characteristic
+  BLE.addService(fallDetectionService); // Add the battery service
+  probabilityFloat.setValue(0.0); // set initial value for this characteristic
+
+  /* Start advertising BLE.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("Bluetooth device active, waiting for connections...");
 }
 
 void loop() {
+  // wait for a BLE central
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    while (central.connected()) {
+      float aX, aY, aZ;
+      
+      // wait for significant motion
+      while (central.connected() and samplesRead == numSamples) {
+        if (IMU.accelerationAvailable()) {
+          // read the acceleration data
+          IMU.readAcceleration(aX, aY, aZ);
+    
+          // sum up the absolutes
+          float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+    
+          // check if it's above the threshold
+          if (aSum >= accelerationThreshold) {
+             // reset the sample read count
+             samplesRead = 0;
+            break;
+          }
+        }
+      }
+    
+      // run tensorflow model, when significant motion is detected
+      while (central.connected() and samplesRead < numSamples) {
+        updateFallProbability();
+      }
+   }
+  
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void updateFallProbability() {
+  /* Read the current voltage level on the A0 analog input pin.
+     This is used here to simulate the charge level of a battery.
+  */
   float aX, aY, aZ, gX, gY, gZ;
 
-  // wait for significant motion
-  while (samplesRead == numSamples) {
-    if (IMU.accelerationAvailable()) {
-      // read the acceleration data
-      IMU.readAcceleration(aX, aY, aZ);
+  // check if new acceleration AND gyroscope data is available
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+    // read the acceleration and gyroscope data
+    IMU.readAcceleration(aX, aY, aZ);
+    IMU.readGyroscope(gX, gY, gZ);
 
-      // sum up the absolutes
-      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+    // normalize the IMU data between 0 to 1 and store in the model's
+    // input tensor
+    tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
+    tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
+    tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
+    tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
+    tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
+    tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
 
-      // check if it's above the threshold
-      if (aSum >= accelerationThreshold) {
-        // reset the sample read count
-        samplesRead = 0;
-        break;
+    samplesRead++;
+
+    if (samplesRead == numSamples) {
+      // Run inferencing
+      TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+      
+      if (invokeStatus != kTfLiteOk) {
+        Serial.println("Invoke failed!");
+        while (1);
+        return;
       }
-    }
-  }
 
-  // check if the all the required samples have been read since
-  // the last time the significant motion was detected
-  while (samplesRead < numSamples) {
-    // check if new acceleration AND gyroscope data is available
-    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-      // read the acceleration and gyroscope data
-      IMU.readAcceleration(aX, aY, aZ);
-      IMU.readGyroscope(gX, gY, gZ);
+      //float prob = tflOutputTensor -> data.f[0];
+      float gestureProbability = tflOutputTensor->data.f[0];
 
-      // normalize the IMU data between 0 to 1 and store in the model's
-      // input tensor
-      tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
-      tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
-      tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
-      tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
-      tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
-      tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
+      Serial.print("Fall: ");
+      Serial.println(gestureProbability);
 
-      samplesRead++;
-
-      if (samplesRead == numSamples) {
-        // Run inferencing
-        TfLiteStatus invokeStatus = tflInterpreter->Invoke();
-        if (invokeStatus != kTfLiteOk) {
-          Serial.println("Invoke failed!");
-          while (1);
-          return;
-        }
-
-
-        Serial.print("Fall: ");
-        Serial.println(tflOutputTensor->data.f[0], 6);
-
-        Serial.println();
-      }
+      probabilityFloat.setValue(gestureProbability);
     }
   }
 }
